828158cf86cf52f219eda3261f09aee292619719
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-03-03 11:30:39 +0100

    feat: add dark mode toggle

--- main.js
+++ main.js
@@ -54,3 +54,9 @@ Catalog.render({
     }
   ]
 }, document.getElementById('catalog'));
+
+const body = document.querySelector('body');
+const toggle = document.querySelector('#catalog-theme-toggle > input');
+toggle.addEventListener('change', () => {
+  body.classList.toggle('theme-dark')
+});
