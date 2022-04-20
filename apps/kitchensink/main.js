2068533a18fb4604bc739c7b3f4aada570ddc275
Arman <arman@appwrite.io>
2022-04-20 13:00:37 +0200

    added app layout

--- apps/kitchensink/main.js
+++ apps/kitchensink/main.js
@@ -6,3 +6,11 @@ const toggle = document.querySelector("#catalog-theme-toggle > input");
 toggle.addEventListener("change", () => {
   body.classList.toggle("theme-dark");
 });
+
+const menuButton = document.querySelector("#menuButton");
+const main = document.querySelector("#main");
+const nav = document.querySelector("#nav");
+menuButton.addEventListener("click", () => {
+  main.classList.toggle("is-open");
+  nav.classList.toggle("nav-top");
+});
