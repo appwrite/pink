d0d3c0c1f24b4702155069632de9b7d573bfdaec
Arman <arman@appwrite.io>
2022-04-27 12:39:07 +0200

    fix: routing to secondary pages

--- apps/kitchensink/vite.config.js
+++ apps/kitchensink/vite.config.js
@@ -5,9 +5,15 @@ const config = {
   server: {
     port: 3030,
   },
-  publicDir: "docs",
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        index: new URL("./index.html", import.meta.url).pathname,
+        settings: new URL("./settings.html", import.meta.url).pathname,
+        user: new URL("./users.html", import.meta.url).pathname,
+      },
+    },
   },
   css: {
     devSourcemap: true,
