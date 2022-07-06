387b01b3811662eb3dc464f32d47730d9102e64f
Arman <arman@appwrite.io>
2022-07-06 09:26:47 +0200

    feat: add new bucket settings page

--- apps/kitchensink/vite.config.js
+++ apps/kitchensink/vite.config.js
@@ -20,6 +20,8 @@ const config = {
         auth: new URL("./auth.html", import.meta.url).pathname,
         auth2: new URL("./auth2.html", import.meta.url).pathname,
         teamActivity: new URL("./teamActivity.html", import.meta.url).pathname,
+        bucketSettings: new URL("./bucketSettings.html", import.meta.url)
+          .pathname,
       },
     },
   },
