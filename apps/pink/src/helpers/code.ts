77a76c0cc8e2973f2490a192b9b0ccf09a3da7b0
tglide <26071571+TGlide@users.noreply.github.com>
2023-01-04 18:16:18 +0000

    feat: getting started page

---
+++ apps/pink/src/helpers/code.ts
@@ -0,0 +1,65 @@
+import Prism from "prismjs";
+
+/**
+ * Receives a string and returns a wrapped string, with max
+ * n characters per line, and a given indentation before each line.
+ */
+function wrapStr(str: string, n: number, indent: string): string {
+  // remove line breaks and tabs
+  str = str.replaceAll(/[\r\n\t]/g, "");
+  const words = str.split(/\s/);
+  const lines = words.reduce((acc, word) => {
+    const lastLine = acc[acc.length - 1];
+
+    if (acc.length === 0 || lastLine.length + word.length + 1 > n) {
+      return [...acc, indent + word];
+    }
+
+    return [...acc.slice(0, -1), lastLine + " " + word];
+  }, [] as string[]);
+
+  return lines.join("\r");
+}
+
+export function format(html: string) {
+  let formatted = "";
+  let indent = "";
+
+  html.split(/>\s*</).forEach((element) => {
+    // Decrease indent if line is a closing tag
+    if (element.match(/^\/\w/)) indent = indent.substring(2);
+
+    // By default, we just add the element with the current indent before it
+    let toConcatenate = indent + "<" + element + ">\r";
+
+    // If the element is too long, wrap its content
+    const maxLineLength = 100;
+    const contentMatch = element.match(/(.*?>)(.*?)(<.*)/s);
+    if (contentMatch && toConcatenate.length > maxLineLength) {
+      const [_, left, content, right] = contentMatch;
+      toConcatenate =
+        indent +
+        "<" +
+        left +
+        "\n" +
+        wrapStr(content, maxLineLength, indent + "  ") +
+        "\r" +
+        indent +
+        right +
+        ">\r";
+    }
+
+    formatted += toConcatenate;
+
+    // Increase indent if element is a non-self-closing tag and not an input tag
+    if (element.match(/^<?\w[^>]*[^\/]$/) && !element.startsWith("input")) {
+      indent += "  ";
+    }
+  });
+
+  return formatted.substring(1, formatted.length - 2);
+}
+
+export function highlight(code: string, language: string) {
+  return Prism.highlight(format(code), Prism.languages[language], language);
+}
