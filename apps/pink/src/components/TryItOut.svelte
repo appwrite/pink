ed31a26dbc8902d6d74360ec8a86122483c70bd1
tglide <26071571+TGlide@users.noreply.github.com>
2023-01-09 21:14:28 +0000

    feat: finish try it out

--- apps/pink/src/components/TryItOut.svelte
+++ apps/pink/src/components/TryItOut.svelte
@@ -1,13 +1,13 @@
-<script>
+<script lang="ts">
   import CodePreview from "./CodePreview.svelte";
 
-  const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+  const capitalize = (str: string) =>
+    str.charAt(0).toUpperCase() + str.slice(1);
   const options = ["card", "form", "alert"];
 
-  let selected = "card";
-
-  const codes = {
-    card: `<article class="card u-text-center">
+  const codes: Record<string, string | Array<{ name: string; code: string }>> =
+    {
+      card: `<article class="card u-text-center">
   <img
     class="avatar is-size-x-large u-margin-inline-auto"
     src="/images/kristin.png"
@@ -21,7 +21,7 @@
     <span class="text">Edit profile</span>
   </button>
 </article>`,
-    form: `<div class="card">
+      form: `<div class="card">
             <div class="u-flex u-main-space-between u-cross-center">
               <h6 class="heading-level-6">Edit Profile</h6>
               <button class="icon-x"></button>
@@ -32,22 +32,22 @@
                   <label class="label" for="name">Name</label>
                   <div class="input-text-wrapper">
                     <input
-                      type="email"
-                      class="input-text u-padding-inline-end-56"
-                      placeholder="Kristin Watson"
-                      name="name"
-                    />
+            type="email"
+            class="input-text u-padding-inline-end-56"
+            placeholder="Kristin Watson"
+            name="name"
+          />
                   </div>
                 </li>
                 <li class="form-item">
                   <label class="label" for="email">Email</label>
                   <div class="input-text-wrapper">
                     <input
-                      type="email"
-                      class="input-text u-padding-inline-end-56"
-                      placeholder="kristin@appwrite.io"
-                      name="email"
-                    />
+            type="email"
+            class="input-text u-padding-inline-end-56"
+            placeholder="kristin@appwrite.io"
+            name="email"
+          />
                   </div>
                 </li>
               </ul>
@@ -59,8 +59,139 @@
               </div>
             </form>
           </div>`,
-    alert: "<div>Alert</div>",
-  };
+      alert: [
+        {
+          name: "Default",
+          code: `<section class="alert-sticky">
+  <button class="x-button" aria-label="close alert">
+    <span class="icon-x" aria-hidden="true" />
+  </button>
+  <div class="alert-sticky-image">
+    <span class="icon-info" aria-hidden="true" />
+  </div>
+  <div class="alert-sticky-content">
+    <h4 class="alert-sticky-title">Alert Sticky Title</h4>
+    <p>Provide contextual feedback messages for typical user actions.</p>
+  </div>
+  <div class="buttons u-flex">
+    <button class="button is-text is-small">
+      <span class="text">Action 1</span>
+    </button>
+    <button class="button is-text is-small">
+      <span class="text">Action 2</span>
+    </button>
+  </div>
+</section>`,
+        },
+        {
+          name: "Success",
+          code: `<section class="alert-sticky is-success">
+  <button class="x-button" aria-label="close alert">
+    <span class="icon-x" aria-hidden="true" />
+  </button>
+  <div class="alert-sticky-image">
+    <span class="icon-info" aria-hidden="true" />
+  </div>
+  <div class="alert-sticky-content">
+    <h4 class="alert-sticky-title">Alert Sticky Title</h4>
+    <p>Provide contextual feedback messages for typical user actions.</p>
+  </div>
+  <div class="buttons u-flex">
+    <button class="button is-text is-small">
+      <span class="text">Action 1</span>
+    </button>
+    <button class="button is-text is-small">
+      <span class="text">Action 2</span>
+    </button>
+  </div>
+</section>`,
+        },
+        {
+          name: "Warning",
+          code: `<section class="alert-sticky is-warning">
+  <button class="x-button" aria-label="close alert">
+    <span class="icon-x" aria-hidden="true" />
+  </button>
+  <div class="alert-sticky-image">
+    <span class="icon-info" aria-hidden="true" />
+  </div>
+  <div class="alert-sticky-content">
+    <h4 class="alert-sticky-title">Alert Sticky Title</h4>
+    <p>Provide contextual feedback messages for typical user actions.</p>
+  </div>
+  <div class="buttons u-flex">
+    <button class="button is-text is-small">
+      <span class="text">Action 1</span>
+    </button>
+    <button class="button is-text is-small">
+      <span class="text">Action 2</span>
+    </button>
+  </div>
+</section>`,
+        },
+        {
+          name: "Info",
+          code: `<section class="alert-sticky is-info">
+  <button class="x-button" aria-label="close alert">
+    <span class="icon-x" aria-hidden="true" />
+  </button>
+  <div class="alert-sticky-image">
+    <span class="icon-info" aria-hidden="true" />
+  </div>
+  <div class="alert-sticky-content">
+    <h4 class="alert-sticky-title">Alert Sticky Title</h4>
+    <p>Provide contextual feedback messages for typical user actions.</p>
+  </div>
+  <div class="buttons u-flex">
+    <button class="button is-text is-small">
+      <span class="text">Action 1</span>
+    </button>
+    <button class="button is-text is-small">
+      <span class="text">Action 2</span>
+    </button>
+  </div>
+</section>`,
+        },
+        {
+          name: "Danger",
+          code: `<section class="alert-sticky is-danger">
+  <button class="x-button" aria-label="close alert">
+    <span class="icon-x" aria-hidden="true" />
+  </button>
+  <div class="alert-sticky-image">
+    <span class="icon-info" aria-hidden="true" />
+  </div>
+  <div class="alert-sticky-content">
+    <h4 class="alert-sticky-title">Alert Sticky Title</h4>
+    <p>Provide contextual feedback messages for typical user actions.</p>
+  </div>
+  <div class="buttons u-flex">
+    <button class="button is-text is-small">
+      <span class="text">Action 1</span>
+    </button>
+    <button class="button is-text is-small">
+      <span class="text">Action 2</span>
+    </button>
+  </div>
+</section>`,
+        },
+      ],
+    };
+
+  let codeKey = "card";
+  let codeIdx = 0;
+
+  $: code = (function getCode() {
+    const selected = codes[codeKey];
+    if (typeof selected === "string") return selected;
+    return selected[codeIdx]?.code;
+  })();
+
+  $: codeExamples = (function getOptions() {
+    const selected = codes[codeKey];
+    if (typeof selected === "string") return null;
+    return selected;
+  })();
 </script>
 
 <section class="try-it-out-section u-margin-block-start-300">
@@ -72,8 +203,8 @@
           <li class="drop-tabs-item">
             <button
               class="drop-tabs-button"
-              on:click={() => (selected = option)}
-              disabled={selected === option}
+              on:click={() => (codeKey = option)}
+              disabled={codeKey === option}
             >
               <span class="text">{capitalize(option)}</span>
             </button>
@@ -83,9 +214,34 @@
 
       <div class="u-container-inline-size u-margin-block-start-8">
         <div class="card u-flex u-main-center u-cross-center" id="tryItCard">
-          {@html codes[selected]}
+          {@html code}
         </div>
       </div>
+      {#if codeExamples}
+        <div class="button-grid">
+          <button
+            class="u-flex u-main-start  u-cross-center"
+            on:click={() => codeIdx > 0 && codeIdx--}
+          >
+            {#if codeIdx > 0}
+              <div class="icon-cheveron-left" />
+              {codeExamples[codeIdx - 1].name}
+            {/if}
+          </button>
+          <span>{codeExamples[codeIdx].name}</span>
+          <button
+            class="u-flex u-main-end u-cross-center"
+            on:click={() => {
+              codeExamples && codeIdx < codeExamples?.length - 1 && codeIdx++;
+            }}
+          >
+            {#if codeIdx < codeExamples.length - 1}
+              {codeExamples[codeIdx + 1].name}
+              <div class="icon-cheveron-right" />
+            {/if}
+          </button>
+        </div>
+      {/if}
     </div>
     <div class="grid-1-1-col-2">
       <section class="apple-window u-min-height-100-percents">
@@ -97,8 +253,8 @@
         <div
           class="box u-overflow-hidden u-border-width-0 u-margin-block-start-12"
         >
-          {#key selected}
-            <CodePreview code={codes[selected]} language="html" lineNumbers />
+          {#key code}
+            <CodePreview {code} language="html" lineNumbers />
           {/key}
         </div>
       </section>
@@ -130,15 +286,20 @@
   #tryItCard {
     --p-card-bg-color-default: var(--color-neutral-5);
     min-block-size: pxToRem(416);
+    margin-bottom: 12px;
 
     :global(#{$theme-dark}) & {
       --p-card-bg-color-default: var(--color-neutral-400);
     }
   }
 
-  // @container (max-width: pxToRem(500)) {
-  //   #tryItCard {
-  //     min-block-size: pxToRem(304);
-  //   }
-  // }
+  .button-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
+
+    padding: 0 16px;
+  }
 </style>
