bb7fc14ff17ce1189ed3331f9ad2db995cf1f437
tglide <26071571+TGlide@users.noreply.github.com>
2023-01-11 20:00:33 +0000

    refactor: avatar labels

--- apps/pink/src/components/homepage/TryItOut.svelte
+++ apps/pink/src/components/homepage/TryItOut.svelte
@@ -202,8 +202,8 @@
 
 <section class="try-it-out-section u-margin-block-start-300">
   <h2 class="heading-level-4">Try It Out Yourself</h2>
-  <div class="grid-1-1 u-margin-block-start-48">
-    <div class="grid-1-1-col-1">
+  <div class="grid u-margin-block-start-48">
+    <div>
       <ul class="drop-tabs is-large">
         {#each options as option}
           <li class="drop-tabs-item">
@@ -234,7 +234,7 @@
               {codeExamples[codeIdx - 1].name}
             {/if}
           </button>
-          <span>{codeExamples[codeIdx].name}</span>
+          <span />
           <button
             class="u-flex u-main-end u-cross-center"
             on:click={() => {
@@ -249,12 +249,19 @@
         </div>
       {/if}
     </div>
-    <div class="grid-1-1-col-2">
+    <div>
       <section class="apple-window u-min-height-100-percents">
-        <header class="u-flex u-gap-8">
-          <div class="apple-window-button is-red" />
-          <div class="apple-window-button is-yellow" />
-          <div class="apple-window-button is-green" />
+        <header>
+          <div class="u-flex u-gap-8">
+            <div class="apple-window-button is-red" />
+            <div class="apple-window-button is-yellow" />
+            <div class="apple-window-button is-green" />
+          </div>
+          {#if codeExamples}
+            <div class="tag">
+              {codeExamples[codeIdx].name}
+            </div>
+          {/if}
         </header>
         <div
           class="box u-direction-ltr u-overflow-hidden u-border-width-0 u-margin-block-start-12"
@@ -289,6 +296,11 @@
     }
   }
 
+  .grid {
+    display: grid;
+    grid-template-columns: 45% 1fr;
+  }
+
   #tryItCard {
     --p-card-bg-color-default: var(--color-neutral-5);
     min-block-size: pxToRem(416);
@@ -307,5 +319,17 @@
     text-align: center;
 
     padding: 0 16px;
+  }
+
+  header {
+    position: relative;
+    padding-block: pxToRem(8);
+
+    .tag {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
   }
 </style>
