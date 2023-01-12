2f77d3be010404a6e5670e8282245ce032d704e4
tglide <26071571+TGlide@users.noreply.github.com>
2023-01-12 18:33:20 +0000

    feat: search

--- apps/pink/search.mjs
+++ apps/pink/search.mjs
@@ -12,10 +12,10 @@ function readHtmlContent(path) {
   // remove style tags
   content = content.replace(/<style.*?>.*?<\/style>/gs, "");
   // remove html tags
-  content = content.replace(/<.*?>/gs, "");
+  content = content.replace(/<.*?>/gs, " ");
 
   // remove newlines
-  content = content.replace(/\n/g, "");
+  content = content.replace(/\n/g, " ");
   // remove extra spaces
   content = content.replace(/\s\s+/g, " ");
 
@@ -27,14 +27,16 @@ const documents = paths.map((path, index) => {
   const content = readHtmlContent(path);
   // Get title from last path folders, excluding dist. e.g. dist/components/avatar/index.html
   // should return 'Components - Avatar'
-  const title = path.split("/").slice(1, -1).join(" - ") || "home";
+  const title =
+    path.replace("-", " ").split("/").slice(1, -1).join(" - ") || "home";
+  const url = path.replace("dist", "").replace("/index.html", "");
 
-  return { id: index, title, content };
+  return { id: index, title, content, url };
 });
 
 let miniSearch = new MiniSearch({
   fields: ["title", "content"], // fields to index for full-text search
-  storeFields: ["title", "content"], // fields to return with search results
+  storeFields: ["title", "content", "url"], // fields to return with search results
 });
 
 await miniSearch.addAllAsync(documents);
--- apps/pink/src/components/Search.svelte
+++ apps/pink/src/components/Search.svelte
@@ -3,6 +3,46 @@
   import MiniSearch from "minisearch";
 
   let searchIndex: MiniSearch | null = null;
+  let search = "";
+  $: searchResults = searchIndex
+    ? searchIndex.search(search, { prefix: true })
+    : [];
+
+  let selectedResult = -1;
+  $: {
+    // Reset selected result when results change
+    searchResults;
+    selectedResult = -1;
+  }
+
+  $: (function keepSelectedInView() {
+    if (selectedResult < 0) {
+      resultsEl?.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+      return;
+    }
+    // Scroll to selected result
+    const selectedEl = resultsEl.querySelector(
+      `a:nth-child(${selectedResult + 1})`
+    ) as HTMLDivElement;
+    if (selectedEl) {
+      // if not in view, scroll to it
+      const inView =
+        selectedEl.offsetTop >= resultsEl.scrollTop &&
+        selectedEl.offsetTop + selectedEl.offsetHeight <=
+          resultsEl.scrollTop + resultsEl.offsetHeight;
+
+      if (inView) return;
+      resultsEl.scrollTo({
+        top: selectedEl.offsetTop - resultsEl.offsetHeight / 2,
+        behavior: "smooth",
+      });
+    }
+  })();
+
+  let resultsEl: HTMLDivElement;
 
   onMount(async () => {
     const res = await fetch("/search.json");
@@ -14,13 +54,54 @@
     });
   });
 
-  let search = "";
-  $: searchResults = searchIndex
-    ? searchIndex.search(search, { prefix: true })
-    : [];
+  const capitalize = (str: string) => {
+    // capitalize all words
+    return str
+      .split(" ")
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(" ");
+  };
 
-  $: console.log(searchResults);
+  function getMatchSnippet(content: string, match: string, maxLength = 60) {
+    const matchIndex = content.toLowerCase().indexOf(match.toLowerCase());
+    if (matchIndex === -1 || match.length > maxLength) return null;
+
+    const availableLength = maxLength - match.length;
+
+    const start = Math.max(0, matchIndex - availableLength / 2);
+    const end = Math.min(
+      content.length,
+      matchIndex + match.length + availableLength / 2
+    );
+
+    const left = content.slice(start, matchIndex);
+    const middle = content.slice(matchIndex, matchIndex + match.length);
+    const right = content.slice(matchIndex + match.length, end);
+
+    return [left, middle, right];
+  }
+
+  function onKeyDown(e: KeyboardEvent) {
+    // If arrow down
+    if (e.key === "ArrowDown") {
+      e.preventDefault();
+      selectedResult = Math.min(selectedResult + 1, searchResults.length - 1);
+    } else if (e.key === "ArrowUp") {
+      // If arrow up
+      e.preventDefault();
+      selectedResult = Math.max(selectedResult - 1, -1);
+    } else if (e.key === "Enter") {
+      // If enter
+      if (selectedResult !== -1) {
+        e.preventDefault();
+        console.log(searchResults[selectedResult].url);
+        window.location.href = searchResults[selectedResult].url || "/";
+      }
+    }
+  }
 </script>
+
+<svelte:window on:keydown={onKeyDown} />
 
 <div class="wrapper">
   <div class="input-text-wrapper is-with-end-button">
@@ -36,17 +117,26 @@
       <span class="icon-x" aria-hidden="true" />
     </button>
   </div>
-  {#if searchIndex}
-    <div class="results">
-      {#each searchResults as result}
-        <div>
-          {result.id}
-          {result.title}
-          {JSON.stringify(result)}
-        </div>
-      {/each}
-    </div>
-  {/if}
+
+  <div class="results box" class:u-hide={!search} bind:this={resultsEl}>
+    {#if !searchResults.length}
+      <p>No results found</p>
+    {/if}
+    {#each searchResults as result, idx}
+      {@const snippet = getMatchSnippet(result.content, search)}
+      <a class:selected={selectedResult === idx} href={result.url}>
+        <span>{capitalize(result.title)}</span>
+
+        {#if snippet}
+          <p class="content">
+            <span>...{snippet[0]}</span>
+            <b>{snippet[1]}</b>
+            <span>{snippet[2]}...</span>
+          </p>
+        {/if}
+      </a>
+    {/each}
+  </div>
 </div>
 
 <style lang="scss">
@@ -55,7 +145,6 @@
     top: 50%;
     left: 50%;
     translate: -50% -50%;
-    border: 1px solid red;
 
     width: 600px;
     max-width: calc(100% - 2rem);
@@ -63,6 +152,9 @@
     max-height: calc(100% - 2rem);
 
     overflow: hidden;
+
+    display: flex;
+    flex-direction: column;
   }
 
   .input-text-wrapper .icon-search {
@@ -71,7 +163,41 @@
   }
 
   .results {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+
     overflow: auto;
     max-height: 100%;
+    margin-top: 16px;
+
+    a {
+      display: flex;
+      flex-direction: column;
+
+      > :nth-child(1) {
+        font-weight: bold;
+      }
+
+      &:hover,
+      &.selected {
+        > :nth-child(1) {
+          text-decoration: underline;
+        }
+      }
+
+      .content {
+        display: flex;
+        white-space: pre;
+
+        span {
+          opacity: 0.5;
+        }
+
+        b {
+          opacity: 0.75;
+        }
+      }
+    }
   }
 </style>
