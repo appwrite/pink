58d60c836e47e67b8f876b9aa7301e5733473d26
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-03-13 16:30:26 +0530

    test: try lost pixel

---
+++ lostpixel.config.ts
@@ -0,0 +1,11 @@
+import { CustomProjectConfig } from "lost-pixel";
+
+export const config: CustomProjectConfig = {
+    pageShots: {
+        pages: [{ path: "/", name: "kitchensink" }],
+        // IP should be localhost when running locally & 172.17.0.1 when running in GitHub action
+
+        baseUrl: "http://localhost:3031",
+    },
+    generateOnly: true,
+};
