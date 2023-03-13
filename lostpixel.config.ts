f45f130009880a6c68309b17812501826156ce4e
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-03-13 17:52:55 +0530

    ci: default pageparams

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,4 +1,8 @@
-import { CustomProjectConfig } from "lost-pixel";
+import { CustomProjectConfig, PageScreenshotParameter } from "lost-pixel";
+
+const defaultPageOptions: Partial<PageScreenshotParameter> = {
+    waitBeforeScreenshot: 2500
+}
 
 export const config: CustomProjectConfig = {
     pageShots: {
@@ -6,14 +10,17 @@ export const config: CustomProjectConfig = {
             {
                 path: "/elements/button",
                 name: "buttons",
+                ...defaultPageOptions
             },
             {
                 path: "/elements/card",
                 name: "card",
+                ...defaultPageOptions
             },
             {
                 path: "/elements/input-field",
                 name: "input-field",
+                ...defaultPageOptions
             },
         ],
         baseUrl: "http://172.17.0.1:3333",
