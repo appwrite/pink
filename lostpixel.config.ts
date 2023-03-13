14292562ca236476ce34429a680cc92684526620
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-03-13 16:51:21 +0530

    fix: tests

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,10 +2,23 @@ import { CustomProjectConfig } from "lost-pixel";
 
 export const config: CustomProjectConfig = {
     pageShots: {
-        pages: [{ path: "/", name: "kitchensink" }],
+        pages: [
+            {
+                path: "/elements/button",
+                name: "buttons",
+            },
+            {
+                path: "/elements/card",
+                name: "card",
+            },
+            {
+                path: "/elements/input-field",
+                name: "input-field",
+            },
+        ],
         // IP should be localhost when running locally & 172.17.0.1 when running in GitHub action
 
-        baseUrl: "http://172.17.0.1:4173",
+        baseUrl: "http://172.17.0.1:3333",
     },
     generateOnly: true,
 };
