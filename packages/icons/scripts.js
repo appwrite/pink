9f217261b4d971891f7fb671749b9a4cd7b3d4ba
Aman <dv451197@gmail.com>
2023-12-21 23:18:51 +0530

    fixed icons size

--- packages/icons/scripts.js
+++ packages/icons/scripts.js
@@ -5,36 +5,36 @@ import { resolve } from "path";
 const src = resolve(process.cwd(), "svg");
 
 export const generateIcons = async () => {
-    await svgtofont({
-        src: src,
-        dist: resolve(process.cwd(), "dist"),
-        fontName: "icon",
-        styleTemplates: resolve(process.cwd(), "templates"),
-        css: {
-            fontSize: "16px",
-        },
-        outSVGReact: false,
-        svgicons2svgfont: {
-            centerHorizontally: true,
-            centerVertically: true,
-            fixedWidth: true,
-            fontHeight: 1000,
-            normalize: true,
-            descent: 200
-        },
-        emptyDist: true,
-        generateInfoData: true,
-        svgoOptions: {
-            full: false
-        },
-        useCSSVars: true
-    });
-}
+  await svgtofont({
+    src: src,
+    dist: resolve(process.cwd(), "dist"),
+    fontName: "icon",
+    styleTemplates: resolve(process.cwd(), "templates"),
+    css: {
+      fontSize: "22px",
+    },
+    outSVGReact: false,
+    svgicons2svgfont: {
+      centerHorizontally: true,
+      centerVertically: true,
+      fixedWidth: true,
+      fontHeight: 1000,
+      normalize: true,
+      descent: 200,
+    },
+    emptyDist: true,
+    generateInfoData: true,
+    svgoOptions: {
+      full: false,
+    },
+    useCSSVars: true,
+  });
+};
 
 export const optimizeSVG = async () => {
-    const fixer = new SVGFixer(src, src, {
-        showProgressBar: true
-    });
+  const fixer = new SVGFixer(src, src, {
+    showProgressBar: true,
+  });
 
-    await fixer.fix();
-}
+  await fixer.fix();
+};
