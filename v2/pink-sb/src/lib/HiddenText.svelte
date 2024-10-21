34185d4e5cfc8c809185f6fad4db0d15044c6cc0
ernstmul <ernst.mulders@gmail.com>
2024-10-21 11:28:07 +0200

    Add HiddenText component

---
+++ v2/pink-sb/src/lib/HiddenText.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+    import { IconDuplicate, IconEye, IconEyeOff } from '@appwrite.io/pink-icons-svelte';
+
+    export let text: string;
+    let isVisible: boolean = false;
+    let timeout: NodeJS.Timeout;
+    const AUTOMATIC_HIDE_DURATION_MS = 10000;
+
+    function toggleVisibility() {
+        clearTimeout(timeout);
+        if (isVisible) {
+            isVisible = false;
+        } else {
+            isVisible = true;
+            timeout = setTimeout(() => {
+                isVisible = false;
+            }, AUTOMATIC_HIDE_DURATION_MS);
+        }
+    }
+
+    function copyToClipboard() {
+        navigator.clipboard.writeText(text);
+    }
+</script>
+
+<div class="container">
+    {#if isVisible}
+        <span>{text}</span><button title="Hide text" on:click={toggleVisibility}
+            ><IconEyeOff /></button
+        >
+    {:else}
+        <span>••••••••••</span><button title="Show text" on:click={toggleVisibility}
+            ><IconEye /></button
+        >
+    {/if}
+    <button title="Copy to clipboard" on:click={copyToClipboard}><IconDuplicate /></button>
+</div>
+
+<style>
+    .container {
+        height: 20px;
+        display: flex;
+        border-radius: var(--border-radius-XXS, 4px);
+        align-items: center;
+
+        gap: var(--space-2, 4px);
+        padding: 0 var(--gap-XXS, 4px);
+
+        &:hover {
+            background: var(--color-overlay-neutral-hover, rgba(25, 25, 28, 0.03));
+        }
+    }
+    button {
+        width: var(--icon-size-S, 16px);
+        height: var(--icon-size-S, 16px);
+        flex-shrink: 0;
+        cursor: pointer;
+        color: var(--color-fgcolor-neutral-tertiary);
+    }
+
+    span {
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+        flex-grow: 1;
+
+        color: var(--color-fgColor-neutral-secondary, #56565c);
+
+        /* Desktop/Code M */
+        font-family: var(--font-family-code, 'Fira Code');
+        font-size: var(--font-size-S, 14px);
+        font-style: normal;
+        font-weight: 400;
+        line-height: 140%; /* 19.6px */
+    }
+</style>
---
+++ v2/pink-sb/src/stories/HiddenText.stories.svelte
@@ -0,0 +1,25 @@
+<script context="module" lang="ts">
+    import { HiddenText } from '$lib/index.js';
+    import type { MetaProps } from '@storybook/addon-svelte-csf';
+    import { Story } from '@storybook/addon-svelte-csf';
+
+    export const meta: MetaProps = {
+        title: 'Components/HiddenText',
+        component: HiddenText,
+        args: {
+            text: 'my-secr3t-text'
+        }
+    };
+</script>
+
+<div class="wrapper">
+    <Story name="Default" let:args>
+        <HiddenText {...args} />
+    </Story>
+</div>
+
+<style>
+    .wrapper {
+        width: 200px;
+    }
+</style>
