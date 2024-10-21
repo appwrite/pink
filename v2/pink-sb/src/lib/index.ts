62a1ee61069f39803428eb4b482aabfdfee70974
Arman <arman@appwrite.io>
2024-10-21 16:58:46 +0200

    fix: export popover and bump

--- v2/pink-sb/src/lib/index.ts
+++ v2/pink-sb/src/lib/index.ts
@@ -22,6 +22,7 @@ export { default as Icon } from './Icon.svelte';
 export { default as Pagination } from './Pagination.svelte';
 export { default as Code } from './Code.svelte';
 export { default as InlineCode } from './InlineCode.svelte';
+export { default as Popover } from './Popover.svelte';
 export { Toast } from './toast/index.js';
 export * as Button from './button/index.js';
 export * as Link from './link/index.js';
