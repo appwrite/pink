93e186f2517614f0f60e1ea1e93a3ebfca50a563
Arman <arman@appwrite.io>
2024-10-27 17:37:52 +0100

    feat: wizard

--- v2/pink-sb/src/lib/layout/Wizard.svelte
+++ v2/pink-sb/src/lib/layout/Wizard.svelte
@@ -14,58 +14,65 @@
 
 <section class="wizard">
     <div class="wizard-container">
-        <header>
-            <Stack gap="xl" justifyContent="space-between" direction="row" alignItems="center">
-                <Title size="s">{title}</Title>
-                {#if href}
-                    <LinkButton icon variant="ghost" size="s" {href}>
-                        <Icon icon={IconX} />
-                    </LinkButton>
-                {:else}
-                    <Button icon variant="ghost" size="s" on:click={buttonMethod}>
-                        <Icon icon={IconX} />
-                    </Button>
-                {/if}
-            </Stack>
-        </header>
-        <div class="wizard-content" class:invert-columns={invertColumns}>
-            <main>
-                <slot />
-            </main>
-            <aside>
-                <slot name="aside" />
-            </aside>
+        <div>
+            <header>
+                <Stack gap="xl" justifyContent="space-between" direction="row" alignItems="center">
+                    <Title size="s">{title}</Title>
+                    {#if href}
+                        <LinkButton icon variant="ghost" size="s" {href}>
+                            <Icon icon={IconX} />
+                        </LinkButton>
+                    {:else}
+                        <Button icon variant="ghost" size="s" on:click={buttonMethod}>
+                            <Icon icon={IconX} />
+                        </Button>
+                    {/if}
+                </Stack>
+            </header>
+            <div class="wizard-content" class:invert-columns={invertColumns}>
+                <main>
+                    <slot />
+                </main>
+                <aside>
+                    <slot name="aside" />
+                </aside>
+            </div>
         </div>
+        {#if $$slots.footer}
+            <footer>
+                <slot name="footer" />
+            </footer>
+        {/if}
     </div>
-    {#if $$slots.footer}
-        <footer>
-            <slot name="footer" />
-        </footer>
-    {/if}
 </section>
 
 <style lang="scss">
     .wizard {
-        display: block;
+        display: flex;
+        align-items: stretch;
+        justify-content: center;
         min-block-size: 100dvh;
         padding-inline: var(--space-10);
         background-color: var(--color-bgcolor-neutral-primary);
 
         &-container {
             max-inline-size: 1440px;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+            justify-content: space-between;
         }
 
         &-content {
             display: flex;
             gap: 2rem;
-            padding-block-start: 1rem;
+            height: 100%;
 
             @media (max-width: 1127.99px) {
                 flex-direction: column;
             }
-            @media (min-width: 768px) {
-                padding-block: 4rem;
-            }
+
             main {
                 flex: 2.25;
             }
@@ -79,6 +86,7 @@
         }
 
         header {
+            margin-block-end: 2rem;
             position: sticky;
             z-index: 1;
             inset-block-start: 0;
@@ -100,7 +108,6 @@
             gap: 1rem;
             justify-content: flex-end;
             padding-block: 1rem;
-            margin-block-start: 1rem;
             border-block-start: 1px solid var(--color-border-neutral);
             background-color: var(--color-bgcolor-neutral-primary);
             @media (max-width: 767.99px) {
