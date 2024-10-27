9cb07a10e66f4393937ef11be3d4c511c4fd74a5
Arman <arman@appwrite.io>
2024-10-27 16:27:41 +0100

    feat: wizard

--- v2/pink-sb/src/lib/layout/Wizard.svelte
+++ v2/pink-sb/src/lib/layout/Wizard.svelte
@@ -1,15 +1,46 @@
+<script>
+    import Title from '$lib/typography/Title.svelte';
+    import Stack from '$lib/layout/Stack.svelte';
+    import Icon from '$lib/Icon.svelte';
+    import { IconX } from '@appwrite.io/pink-icons-svelte';
+    import Button from '$lib/button/Button.svelte';
+    import LinkButton from '$lib/button/Anchor.svelte';
+
+    export let title = '';
+    export let buttonMethod = () => {};
+    export let href = '';
+    export let invertColumns = false;
+</script>
+
 <section class="wizard">
     <div class="wizard-container">
         <header>
-            <slot name="header" />
+            <Stack gap="xl" justifyContent="space-between" direction="row" alignItems="center">
+                <Title size="s">{title}</Title>
+                {#if href}
+                    <LinkButton icon variant="ghost" size="s" {href}>
+                        <Icon icon={IconX} />
+                    </LinkButton>
+                {:else}
+                    <Button icon variant="ghost" size="s" on:click={buttonMethod}>
+                        <Icon icon={IconX} />
+                    </Button>
+                {/if}
+            </Stack>
         </header>
-        <main>
-            <slot />
-        </main>
-        <aside>
-            <slot name="aside" />
-        </aside>
-        <footer></footer>
+        <div class="wizard-content" class:invert-columns={invertColumns}>
+            <main>
+                <slot />
+            </main>
+            <aside>
+                <slot name="aside" />
+            </aside>
+        </div>
+        {#if $$slots.footer}
+            <footer>
+                <slot name="footer" />
+            </footer>
+        {/if}
     </div>
 </section>
 
@@ -23,6 +54,26 @@
         &-container {
             max-inline-size: 1440px;
         }
+
+        &-content {
+            display: flex;
+            gap: 2rem;
+            padding-block-start: 1rem;
+            @media (min-width: 768px) {
+                padding-block: 4rem;
+            }
+            main {
+                flex: 2.25;
+            }
+            aside {
+                flex: 1;
+            }
+
+            &.invert-columns {
+                flex-direction: row-reverse;
+            }
+        }
+
         header {
             position: sticky;
             z-index: 1;
@@ -43,8 +94,8 @@
             inset-block-end: 0;
             display: flex;
             gap: 1rem;
-            justify-content: space-between;
-            padding-block: 2rem;
+            justify-content: flex-end;
+            padding-block: 1rem;
             border-block-start: 1px solid var(--color-border-neutral);
             background-color: var(--color-bgcolor-neutral-primary);
         }
--- v2/pink-sb/src/stories/layout/Wizard.stories.svelte
+++ v2/pink-sb/src/stories/layout/Wizard.stories.svelte
@@ -23,18 +23,32 @@
 
 <script>
     import { Card, Typography } from '$lib/index.ts';
+    import Button from '$lib/button/Button.svelte';
     import { Story, Template } from '@storybook/addon-svelte-csf';
+    import Stack from './Stack.stories.svelte';
 </script>
 
 <Template let:args>
     <Layout.Wizard {...args}></Layout.Wizard>
 </Template>
 
-<Story name="Default" args={{ direction: 'row' }} let:args>
+<Story name="Default" args={{ title: 'Wizard' }} let:args>
     <Layout.Wizard {...args}>
-        <svelte:fragment slot="header">
-            <Typography.Title size="l">Title</Typography.Title>
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+
+        <svelte:fragment slot="aside">
+            <Card.Base>aspdsasdfdf</Card.Base>
         </svelte:fragment>
+        <svelte:fragment slot="footer">
+            <Button variant="secondary" size="s">Cancel</Button>
+            <Button size="s">Next</Button>
+        </svelte:fragment>
+    </Layout.Wizard>
+</Story>
+<Story name="Inverted columns" args={{ title: 'Wizard', invertColumns: true }} let:args>
+    <Layout.Wizard {...args}>
         <div>Item 1</div>
         <div>Item 2</div>
         <div>Item 3</div>
