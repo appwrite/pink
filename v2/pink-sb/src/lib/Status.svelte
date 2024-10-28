12aa172c57621aec1abded0b232506b2afeab19f
Arman <arman@appwrite.io>
2024-10-28 10:43:44 +0100

    fix: status size

--- v2/pink-sb/src/lib/Status.svelte
+++ v2/pink-sb/src/lib/Status.svelte
@@ -5,12 +5,12 @@
     export let label: string;
 </script>
 
-<Stack gap="s" direction="row" alignItems="center">
+<Stack gap="xs" direction="row" alignItems="center">
     <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
         <circle
             cx="6"
             cy="6"
-            r="6"
+            r="4"
             class:status-waiting={status === 'waiting'}
             class:status-ready={status === 'ready'}
             class:status-processing={status === 'processing'}
