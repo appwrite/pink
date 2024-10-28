8c7ecbdccfe92b0ab6e67811066e1fee77ed15ba
Arman <arman@appwrite.io>
2024-10-28 12:29:14 +0100

    fix: stack

--- v2/pink-sb/src/lib/layout/Stack.svelte
+++ v2/pink-sb/src/lib/layout/Stack.svelte
@@ -8,7 +8,7 @@
      * The gap between the stack items.
      * none: 0 | xxxs: 2px | xxs: 4px | xs: 6px | s: 8px | m: 12px | l: 16px | xl: 20px | xxl: 32px
      */
-    export let gap: 'none' | 'xxxs' | 'xxs' | 'xs' | 's' | 'm' | 'l' | 'xl' = 'm';
+    export let gap: 'none' | 'xxxs' | 'xxs' | 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl' = 'm';
     /**
      * The wrap behavior of the stack.
      */
