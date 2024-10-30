22010ebf610c3a478933f22316af82c5314ec11e
ernstmul <ernst.mulders@gmail.com>
2024-10-30 14:05:49 +0100

    Skeleton loader

---
+++ v2/pink-sb/src/lib/Skeleton.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    type $$Props = { variant: 'circle' | 'square' | 'line'; width: number; height?: number };
+
+    export let variant: $$Props['variant'] = 'circle';
+    export let width: $$Props['width'] = 44;
+    export let height: $$Props['height'] = 100;
+</script>
+
+<div
+    class="skeleton"
+    style="width: {width}px; {variant === 'line' && `height: ${height}px;`}"
+    class:circle={variant === 'circle'}
+    class:square={variant === 'square'}
+/>
+
+<style>
+    .skeleton {
+        background-color: var(--color-bgcolor-neutral-tertiary);
+        background-image: linear-gradient(
+            90deg,
+            var(--color-bgcolor-neutral-tertiary) 25%,
+            var(--color-overlay-neutral-hover) 50%,
+            var(--color-bgcolor-neutral-tertiary) 75%
+        );
+        background-size: 200% 100%;
+        animation: loading 2s linear infinite;
+        border-radius: var(--border-radius-xs, 6px);
+    }
+
+    .circle {
+        border-radius: 50%;
+    }
+
+    .circle,
+    .square {
+        aspect-ratio: 1/1;
+    }
+
+    @keyframes loading {
+        0% {
+            background-position: 200% 0;
+        }
+        100% {
+            background-position: -200% 0;
+        }
+    }
+</style>
---
+++ v2/pink-sb/src/stories/Skeleton.stories.svelte
@@ -0,0 +1,43 @@
+<script context="module" lang="ts">
+    import { Skeleton } from '$lib/index.js';
+    import type { MetaProps } from '@storybook/addon-svelte-csf';
+
+    export const meta: MetaProps = {
+        title: 'Components/Skeleton',
+        component: Skeleton,
+        args: {
+            variant: 'circle',
+            width: 44,
+            height: 12
+        },
+        argTypes: {
+            variant: {
+                options: ['circle', 'square', 'line'],
+                control: { type: 'select' }
+            },
+            width: {
+                control: { type: 'number' }
+            },
+            height: {
+                control: { type: 'number' }
+            }
+        }
+    };
+</script>
+
+<script>
+    import { Story } from '@storybook/addon-svelte-csf';
+</script>
+
+<div class="wrapper">
+    <Story name="Default" let:args>
+        <Skeleton {...args} />
+    </Story>
+</div>
+
+<style>
+    .wrapper {
+        margin: 100px auto;
+        width: 400px;
+    }
+</style>
