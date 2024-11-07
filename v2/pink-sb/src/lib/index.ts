e26f8e620a9ea45530fad09eeb8d2c13aab2a404
Arman <arman@appwrite.io>
2024-11-07 16:01:13 +0100

    chore: add spinner to components

--- v2/pink-sb/src/lib/index.ts
+++ v2/pink-sb/src/lib/index.ts
@@ -28,6 +28,7 @@ export { default as Popover } from './Popover.svelte';
 export { default as Fieldset } from './Fieldset.svelte';
 export { default as Image } from './Image.svelte';
 export { default as Modal } from './Modal.svelte';
+export { default as Spinner } from './Spinner.svelte';
 export { Toast } from './toast/index.js';
 export * as Button from './button/index.js';
 export * as Link from './link/index.js';
