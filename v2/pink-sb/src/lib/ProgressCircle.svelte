0efea129bb3f359fba9797c488cd7d2ace8f4a33
ernstmul <ernst.mulders@gmail.com>
2024-11-28 15:45:53 +0100

    Add progress circle component

---
+++ v2/pink-sb/src/lib/ProgressCircle.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+    export let progress: number = 0;
+    export let size: 's' | 'm' | 'l' = 'm';
+
+    function getPixelSize() {
+        switch (size) {
+            case 's':
+                return 24;
+            case 'm':
+                return 36;
+            case 'l':
+                return 48;
+        }
+    }
+</script>
+
+<div>
+    <svg
+        width={getPixelSize()}
+        height={getPixelSize()}
+        viewBox="0 0 {getPixelSize()} {getPixelSize()}"
+        class="circular-progress"
+        style="--progress-target: {progress}; --circle-size: {getPixelSize()}px"
+    >
+        <circle class="bg"></circle>
+        <circle class="fg"></circle>
+    </svg>
+</div>
+
+<style>
+    .circular-progress {
+        --size: var(--circle-size);
+        --half-size: calc(var(--size) / 2);
+        --stroke-width: 2px;
+        --radius: calc((var(--size) - var(--stroke-width)) / 2);
+        --circumference: calc(var(--radius) * pi * 2);
+        --dash: calc((var(--progress) * var(--circumference)) / 100);
+        animation: progress-animation 1s linear 0s 1 forwards;
+    }
+
+    .circular-progress circle {
+        cx: var(--half-size);
+        cy: var(--half-size);
+        r: var(--radius);
+        stroke-width: var(--stroke-width);
+        fill: none;
+        stroke-linecap: round;
+    }
+
+    .circular-progress circle.bg {
+        stroke: var(--color-bgcolor-neutral-tertiary);
+    }
+
+    .circular-progress circle.fg {
+        transform: rotate(-90deg);
+        transform-origin: var(--half-size) var(--half-size);
+        stroke-dasharray: var(--dash) calc(var(--circumference) - var(--dash));
+        transition: stroke-dasharray 0.3s linear 0s;
+        stroke: var(--brand-pink-500);
+    }
+
+    @property --progress {
+        syntax: '<number>';
+        inherits: false;
+        initial-value: 0;
+    }
+
+    @keyframes progress-animation {
+        from {
+            --progress: 0;
+        }
+        to {
+            --progress: var(--progress-target);
+        }
+    }
+</style>
---
+++ v2/pink-sb/src/stories/ProgressCircle.stories.svelte
@@ -0,0 +1,40 @@
+<script context="module" lang="ts">
+    import { ProgressCircle } from '$lib/index.js';
+    import type { MetaProps } from '@storybook/addon-svelte-csf';
+    import { Story, Template } from '@storybook/addon-svelte-csf';
+
+    export const meta: MetaProps = {
+        title: 'Components/ProgressCircle',
+        component: ProgressCircle,
+        args: {
+            progress: 30,
+            size: 'm'
+        },
+        argTypes: {
+            progress: {
+                control: 'number',
+                description: 'Progress percentage'
+            },
+            size: {
+                options: ['s', 'm', 'l'],
+                control: { type: 'select' }
+            }
+        }
+    };
+</script>
+
+<div class="wrapper">
+    <Template let:args>
+        <ProgressCircle {...args} />
+    </Template>
+</div>
+<Story name="Size S" args={{ size: 's' }} />
+<Story name="Size M" args={{ size: 'm' }} />
+<Story name="Size L" args={{ size: 'l' }} />
+
+<style>
+    .wrapper {
+        margin: 100px auto;
+        width: 400px;
+    }
+</style>
