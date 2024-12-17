52b7fc8213931bcb9668ede15434809fc93dbe08
Jesse Winton <jesse@appwrite.io>
2024-12-17 18:33:12 -0500

    update

--- v2/pink-sb/src/lib/lab/InlineInput.svelte
+++ v2/pink-sb/src/lib/lab/InlineInput.svelte
@@ -3,7 +3,10 @@
     import { IconHashtag } from '@appwrite.io/pink-icons-svelte';
     import { onMount, onDestroy } from 'svelte';
 
-    export let label: string = '';
+    let w = 0;
+    $: width = w + 'px';
+
+    // export let label: string = '';
     export let value: string = '';
 
     let open: boolean = false;
@@ -27,12 +30,15 @@
     });
 </script>
 
-<div class="input">
-    <Tooltip offsetAmount={20}>
-        <button class="trigger" on:click={handleOpen}><IconHashtag /></button>
-        <p slot="tooltip">Enter a custom Project ID</p>
-    </Tooltip>
-    <div class="sub">
+<div class="input" style:--width={width}>
+    <div class="sub" bind:clientWidth={w}>
+        <Tooltip offsetAmount={20}>
+            <button class="trigger" on:click={handleOpen}>
+                <span class="icon"><IconHashtag /></span></button
+            >
+            <p slot="tooltip">Enter a custom Project ID</p>
+        </Tooltip>
+
         {#if open}
             <!-- svelte-ignore a11y-autofocus -->
             <input type="text" bind:value autofocus />
@@ -44,24 +50,29 @@
     .input {
         display: flex;
         align-items: center;
-        padding: 12px;
         border-radius: 8px;
         border: 1px solid var(--color-border-neutral-strong);
-        width: fit-content;
-        min-height: 50px;
+        width: var(--width);
+        min-height: 40px;
+        min-width: 40px;
+        justify-content: center;
         transition: width 0.2s ease-out;
 
         .sub {
             display: block;
-            width: fit-content;
         }
 
         .trigger {
-            height: 12px;
-            width: 12px;
             display: flex;
             align-items: center;
-            gap: var(--space-3);
+            justify-content: center;
+            width: 100%;
+            height: 100%;
+
+            .icon {
+                height: 12px;
+                width: 12px;
+            }
         }
     }
 </style>
