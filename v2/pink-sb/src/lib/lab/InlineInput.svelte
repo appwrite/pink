1fc1fe4bd699ce1c161f5cf548163516190f4291
Jesse Winton <jesse@appwrite.io>
2024-12-24 12:48:24 -0800

    Update InlineInput.svelte

--- v2/pink-sb/src/lib/lab/InlineInput.svelte
+++ v2/pink-sb/src/lib/lab/InlineInput.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import Tooltip from '$lib/Tooltip.svelte';
-    import { IconHashtag } from '@appwrite.io/pink-icons-svelte';
-    import { onMount, onDestroy } from 'svelte';
+    import { IconPencil } from '@appwrite.io/pink-icons-svelte';
+    import { slide } from 'svelte/transition';
 
+    let inputWidth = 200; // Set a default expanded width
     let w = 0;
-    $: width = w + 'px';
+    $: width = open ? `${inputWidth}px` : '25px';
 
-    // export let label: string = '';
     export let value: string = '';
 
     let open: boolean = false;
@@ -20,59 +20,84 @@
             open = false;
         }
     };
-
-    onMount(() => {
-        document.addEventListener('keydown', handleKeyDown);
-    });
-
-    onDestroy(() => {
-        document.removeEventListener('keydown', handleKeyDown);
-    });
 </script>
 
-<div class="input" style:--width={width}>
-    <div class="sub" bind:clientWidth={w}>
-        <Tooltip offsetAmount={20}>
-            <button class="trigger" on:click={handleOpen}>
-                <span class="icon"><IconHashtag /></span></button
-            >
-            <p slot="tooltip">Enter a custom Project ID</p>
-        </Tooltip>
+<svelte:window on:keydown={handleKeyDown} />
 
-        {#if open}
-            <!-- svelte-ignore a11y-autofocus -->
-            <input type="text" bind:value autofocus />
-        {/if}
+<div class="input-container" style:--width={width}>
+    <div class="icon-container">
+        <Tooltip offsetAmount={12}>
+            <button class="trigger" on:click={handleOpen}>
+                <span class="icon"><IconPencil /></span>
+            </button>
+            <p slot="tooltip" class="tooltip">Enter a custom Project ID</p>
+        </Tooltip>
     </div>
+
+    {#if open}
+        <div class="input-wrapper" transition:slide={{ duration: 200 }}>
+            <input type="text" bind:value />
+        </div>
+    {/if}
 </div>
 
 <style lang="scss">
-    .input {
+    .input-container {
+        position: relative;
         display: flex;
         align-items: center;
         border-radius: 8px;
         border: 1px solid var(--color-border-neutral-strong);
         width: var(--width);
-        min-height: 40px;
-        min-width: 40px;
-        justify-content: center;
+        min-height: 25px;
         transition: width 0.2s ease-out;
+    }
 
-        .sub {
-            display: block;
-        }
+    .icon-container {
+        position: absolute;
+        z-index: 10;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        z-index: 100;
+        width: 25px;
+        height: 25px;
+    }
 
-        .trigger {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            width: 100%;
-            height: 100%;
+    .input-wrapper {
+        flex-grow: 1;
+        margin-left: 25px;
+    }
 
-            .icon {
-                height: 12px;
-                width: 12px;
-            }
-        }
+    input {
+        width: 100%;
+        height: 100%;
+        border: none;
+        outline: none;
+        padding: 0 8px;
+    }
+
+    .tooltip {
+        font-size: 12px;
+        color: var(--color-text-neutral-strong);
+    }
+
+    .trigger {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+    }
+
+    .icon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 12px;
+        width: 12px;
     }
 </style>
