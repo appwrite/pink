e75670b140865791fecabc9302543dc00d3a7fc3
Arman <arman@appwrite.io>
2025-01-15 10:44:58 +0100

    feat: add table button

---
+++ v2/pink-sb/src/lib/table/Button.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import type { HTMLAnchorAttributes } from 'svelte/elements';
+    type $$Props = HTMLAnchorAttributes & { href: string };
+</script>
+
+<button type="button" role="row" on:click|preventDefault {...$$restProps}>
+    <slot />
+</button>
+
+<style lang="scss">
+    [role='row'] {
+        display: table-row;
+        width: 100%;
+
+        &:hover {
+            background: var(--color-overlay-neutral-hover);
+        }
+
+        &:last-child {
+            :global([role='cell']) {
+                border-bottom: 0;
+            }
+        }
+    }
+</style>
--- v2/pink-sb/src/lib/table/index.ts
+++ v2/pink-sb/src/lib/table/index.ts
@@ -2,6 +2,7 @@ import Root from './Table.svelte';
 import Cell from './Cell.svelte';
 import Row from './Row.svelte';
 import Link from './Link.svelte';
+import Button from './Button.svelte';
 import Header from './header/index.js';
 
 export default {
@@ -9,5 +10,6 @@ export default {
     Cell,
     Row,
     Link,
+    Button,
     Header
 };
