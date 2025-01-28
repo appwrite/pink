78e2c3be870ec08a85f0f02104173fb781276b9e
Torsten Dittmann <torsten.dittmann@googlemail.com>
2025-01-28 23:30:23 +0100

    fix: modal

--- v2/pink-sb/src/lib/Modal.svelte
+++ v2/pink-sb/src/lib/Modal.svelte
@@ -147,6 +147,7 @@
 
         &[open] {
             &::backdrop {
+                opacity: 1;
                 animation: backdrop-enter 150ms ease-in-out forwards;
             }
             section {
