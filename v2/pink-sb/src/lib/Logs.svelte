10592d69ff976cf19c81dd4d9cd806ad2e8814fe
Arman <arman@appwrite.io>
2025-01-30 15:24:55 +0100

    fix: logs copy error

--- v2/pink-sb/src/lib/Logs.svelte
+++ v2/pink-sb/src/lib/Logs.svelte
@@ -3,10 +3,53 @@
     import { IconDuplicate, IconSearch } from '@appwrite.io/pink-icons-svelte';
     import { Button, Card, Icon, Input } from './index.ts';
     import Stack from './layout/Stack.svelte';
-    import { copy } from './helpers.ts';
     import Tooltip from './Tooltip.svelte';
 
     export let logs: string;
+
+    async function securedCopy(value: string) {
+        try {
+            await navigator.clipboard.writeText(value);
+        } catch {
+            return false;
+        }
+
+        return true;
+    }
+
+    function unsecuredCopy(value: string) {
+        const textArea = document.createElement('textarea');
+        textArea.value = value;
+
+        // Avoid scrolling to bottom
+        textArea.style.top = '0';
+        textArea.style.left = '0';
+        textArea.style.position = 'fixed';
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        let success = true;
+        try {
+            document.execCommand('copy');
+        } catch {
+            success = false;
+        } finally {
+            document.body.removeChild(textArea);
+        }
+
+        return success;
+    }
+
+    export async function copy(value: string) {
+        // securedCopy works only in HTTPS environment.
+        // unsecuredCopy works in HTTP and only runs if securedCopy fails.
+        const success = (await securedCopy(value)) || unsecuredCopy(value);
+
+        return success;
+    }
+
     let search = '';
     const fuse = new Fuse(
         logs.split('\n').map((line) => ({ line })),
