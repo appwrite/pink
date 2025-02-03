fc8f20a712aafcefb1fe2c7868c6d18e33eb4562
ernstmul <ernst.mulders@gmail.com>
2025-02-03 13:48:25 +0100

    Set correct viewport

--- v2/pink-sb/src/lib/sidebar/Base.svelte
+++ v2/pink-sb/src/lib/sidebar/Base.svelte
@@ -71,7 +71,7 @@
 
         transition: all 0.2s ease-in-out;
 
-        @media (min-width: 768px) {
+        @media (min-width: 1024px) {
             overflow: visible;
             padding: var(--space-7, 16px) var(--space-5, 10px);
             top: 48px;
