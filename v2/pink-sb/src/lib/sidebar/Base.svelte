423d0d6f3d525097a001fc9cd82b4d822d64b35d
ernstmul <ernst.mulders@gmail.com>
2025-02-04 15:46:55 +0100

    Update paddings

--- v2/pink-sb/src/lib/sidebar/Base.svelte
+++ v2/pink-sb/src/lib/sidebar/Base.svelte
@@ -77,7 +77,7 @@
 
         @media (min-width: 1024px) {
             overflow: visible;
-            padding: var(--space-7, 16px);
+            padding: var(--space-7, 16px) var(--space-5, 10px);
             top: 48px;
             height: calc(100vh - 48px);
         }
