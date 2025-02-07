8a34f074cb5ce6f88c610cd21ccd8b1d33729f4d
Darshan <itznotabug@gmail.com>
2025-02-07 15:04:27 +0530

    update: checkbox state-management on table.

--- v2/pink-sb/src/lib/table/Table.svelte
+++ v2/pink-sb/src/lib/table/Table.svelte
@@ -1,15 +1,62 @@
-<script>
+<script lang="ts">
     import Row from './Row.svelte';
+    import Cell from './Cell.svelte';
+    import Checkbox from '$lib/selector/Checkbox.svelte';
+
+    export let selection = false;
+    export let idKey: string = 'id';
+    export let selectedIds: string[] = [];
+    export let items: Array<Record<string, any>> = [];
+
+    $: someSelected = !allSelected && items.some((item) => selectedIds.includes(item[idKey]));
+    $: allSelected = items.length > 0 && items.every((item) => selectedIds.includes(item[idKey]));
+
+    function toggleAll() {
+        selectedIds = allSelected ? [] : items.map((item) => item[idKey]);
+    }
+
+    function toggleRow(id: string) {
+        if (selectedIds.includes(id)) {
+            selectedIds = selectedIds.filter((selId) => selId !== id);
+        } else {
+            selectedIds = [...selectedIds, id];
+        }
+    }
 </script>
 
 <div class="root">
     <div role="table">
         {#if $$slots.header}
             <Row type="header">
+                {#if selection}
+                    <Cell width="20px">
+                        <Checkbox
+                            size="s"
+                            checked={allSelected ? true : someSelected ? 'indeterminate' : false}
+                            on:click={toggleAll}
+                        />
+                    </Cell>
+                {/if}
                 <slot name="header" />
             </Row>
         {/if}
-        <slot />
+
+        {#if selection}
+            {#each items as tableItem}
+                <Row type="row">
+                    <Cell>
+                        <Checkbox
+                            size="s"
+                            on:click={() => toggleRow(tableItem[idKey])}
+                            checked={selectedIds.includes(tableItem[idKey])}
+                        />
+                    </Cell>
+                    <slot name="row" {tableItem} />
+                </Row>
+            {/each}
+        {:else}
+            <slot />
+        {/if}
     </div>
 </div>
 
