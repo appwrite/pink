f4dce2bd27ae2f1eacaf6abdb61c1a359a7c869c
Arman <arman@appwrite.io>
2025-02-13 10:57:05 +0100

    feat: add new new stack size

--- v2/pink-sb/src/lib/layout/Stack.svelte
+++ v2/pink-sb/src/lib/layout/Stack.svelte
@@ -6,9 +6,9 @@
     export let direction: 'column' | 'row' | 'row-reverse' | 'column-reverse' = 'column';
     /**
      * The gap between the stack items.
-     * none: 0 | xxxs: 2px | xxs: 4px | xs: 6px | s: 8px | m: 12px | l: 16px | xl: 20px | xxl: 32px
+     * none: 0 | xxxs: 2px | xxs: 4px | xs: 6px | s: 8px | m: 12px | l: 16px | xl: 20px | xxl: 32px | xxxl: 40px
      */
-    export let gap: 'none' | 'xxxs' | 'xxs' | 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl' = 'm';
+    export let gap: 'none' | 'xxxs' | 'xxs' | 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl' | 'xxxl' = 'm';
     /**
      * The wrap behavior of the stack.
      */
