0ed307c2eae3a80b1e1782e50d0c299fee73c158
Torsten Dittmann <torsten.dittmann@googlemail.com>
2025-03-10 20:11:17 +0400

    fix: add cell vertical alignments

--- v2/pink-sb/src/lib/table/Cell.svelte
+++ v2/pink-sb/src/lib/table/Cell.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
     export let width: string | undefined = undefined;
+    export let alignment: 'middle' | 'start' | 'end' = 'middle';
 </script>
 
-<div role="cell" style:--cell-width={width}>
+<div role="cell" style:--cell-width={width} style:--cell-alignment={alignment}>
     <slot />
 </div>
 
 <style lang="scss">
     [role='cell'] {
         --p-cell-width: var(--cell-width);
+        --p-cell-alignment: var(--cell-alignment);
         display: table-cell;
-        vertical-align: middle;
+        vertical-align: var(--cell-alignment);
         padding-inline: var(--space-6);
         height: 40px;
         border-bottom: var(--border-width-s) solid var(--border-neutral);
