6373737e9e1a012942be9c167c3312e625914339
Darshan <itznotabug@gmail.com>
2025-03-10 19:11:56 +0530

    update: custom width for action menu root inside popovers.

--- v2/pink-sb/src/lib/action-menu/Root.svelte
+++ v2/pink-sb/src/lib/action-menu/Root.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
+    export let width: string | undefined = undefined;
     export let noPadding: undefined | boolean = false;
 </script>
 
-<div style:--action-menu-root-padding={noPadding ? 0 : 'var(--space-2)'}>
+<div
+        style:--action-menu-root-width={width ? width : '224px'}
+        style:--action-menu-root-padding={noPadding ? 0 : 'var(--space-2)'}
+>
     <slot />
 </div>
 
 <style lang="scss">
     div {
         display: flex;
+        overflow: hidden;
         flex-direction: column;
         padding: var(--action-menu-root-padding);
-        overflow: hidden;
-        min-width: 224px;
+        min-width: var(--action-menu-root-width);
     }
 </style>
