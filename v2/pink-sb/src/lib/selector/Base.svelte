77241853573371170a52050895da330630f35fb1
Darshan <itznotabug@gmail.com>
2025-03-10 09:20:45 +0530

    fix: missing description due to empty forwarded slots.

--- v2/pink-sb/src/lib/selector/Base.svelte
+++ v2/pink-sb/src/lib/selector/Base.svelte
@@ -13,13 +13,13 @@
             <label for={id}>
                 <Typography.Text variant="m-500">{label}</Typography.Text>
             </label>
-            <slot name="description">
-                {#if description}
-                    <Typography.Text variant="m-400" color="--fgcolor-neutral-tertiary"
-                        >{description}</Typography.Text
-                    >
-                {/if}
-            </slot>
+            {#if description}
+                <Typography.Text variant="m-400" color="--fgcolor-neutral-tertiary"
+                    >{description}</Typography.Text
+                >
+            {:else if $$slots.description}
+                <slot name="description"></slot>
+            {/if}
         </Layout.Stack>
     {/if}
 </Layout.Stack>
