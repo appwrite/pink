92b377c02c37bda6057b1453ce89e86b0323213f
Torsten Dittmann <torsten.dittmann@googlemail.com>
2025-03-11 17:07:10 +0400

    fix: cell alignment

--- v2/pink-sb/src/lib/table/Cell.svelte
+++ v2/pink-sb/src/lib/table/Cell.svelte
@@ -3,7 +3,12 @@
     export let alignment: 'middle' | 'start' | 'end' = 'middle';
 </script>
 
-<div role="cell" style:--cell-width={width} style:--cell-alignment={alignment}>
+<div
+    role="cell"
+    style:--cell-width={width}
+    class:vertical-start={alignment === 'start'}
+    class:vertical-end={alignment === 'end'}
+>
     <slot />
 </div>
 
@@ -12,11 +17,19 @@
         --p-cell-width: var(--cell-width);
         --p-cell-alignment: var(--cell-alignment);
         display: table-cell;
-        vertical-align: var(--cell-alignment);
+        vertical-align: center;
         padding-inline: var(--space-6);
         height: 40px;
         border-bottom: var(--border-width-s) solid var(--border-neutral);
         width: var(--p-cell-width);
         white-space: nowrap;
+
+        &.vertical-start {
+            vertical-align: top;
+        }
+
+        &.vertical-end {
+            vertical-align: bottom;
+        }
     }
 </style>
