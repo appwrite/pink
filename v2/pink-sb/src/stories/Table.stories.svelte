62aed048c316adc1da805ea89d13c67d1a2a390d
Torsten Dittmann <torsten.dittmann@googlemail.com>
2025-03-18 18:52:23 +0100

    fix: table story

--- v2/pink-sb/src/stories/Table.stories.svelte
+++ v2/pink-sb/src/stories/Table.stories.svelte
@@ -30,31 +30,6 @@
 <Story name="Default">
     <Table.Root {columns} let:root>
         <svelte:fragment slot="header" let:root>
-            <Table.Header.Cell {root}>Lorem</Table.Header.Cell>
-            <Table.Header.Cell {root}>Ipsum</Table.Header.Cell>
-            <Table.Header.Cell {root}>Dolor</Table.Header.Cell>
-        </svelte:fragment>
-        <Table.Row.Base {root}>
-            <Table.Cell {root}>Lorem</Table.Cell>
-            <Table.Cell {root}>Ipsum</Table.Cell>
-            <Table.Cell {root}>Dolor</Table.Cell>
-        </Table.Row.Base>
-        <Table.Row.Base {root}>
-            <Table.Cell {root}>Lorem</Table.Cell>
-            <Table.Cell {root}>Ipsum</Table.Cell>
-            <Table.Cell {root}>Dolor</Table.Cell>
-        </Table.Row.Base>
-        <Table.Row.Base {root}>
-            <Table.Cell {root}>Lorem</Table.Cell>
-            <Table.Cell {root}>Ipsum</Table.Cell>
-            <Table.Cell {root}>Dolor</Table.Cell>
-        </Table.Row.Base>
-    </Table.Root>
-</Story>
-
-<Story name="Auto Columns">
-    <Table.Root columns={3} let:root>
-        <svelte:fragment slot="header" let:root>
             <Table.Header.Cell column="first" {root}>Lorem</Table.Header.Cell>
             <Table.Header.Cell column="second" {root}>Ipsum</Table.Header.Cell>
             <Table.Header.Cell column="third" {root}>Dolor</Table.Header.Cell>
@@ -73,6 +48,31 @@
             <Table.Cell column="first" {root}>Lorem</Table.Cell>
             <Table.Cell column="second" {root}>Ipsum</Table.Cell>
             <Table.Cell column="third" {root}>Dolor</Table.Cell>
+        </Table.Row.Base>
+    </Table.Root>
+</Story>
+
+<Story name="Auto Columns">
+    <Table.Root columns={3} let:root>
+        <svelte:fragment slot="header" let:root>
+            <Table.Header.Cell {root}>Lorem</Table.Header.Cell>
+            <Table.Header.Cell {root}>Ipsum</Table.Header.Cell>
+            <Table.Header.Cell {root}>Dolor</Table.Header.Cell>
+        </svelte:fragment>
+        <Table.Row.Base {root}>
+            <Table.Cell {root}>Lorem</Table.Cell>
+            <Table.Cell {root}>Ipsum</Table.Cell>
+            <Table.Cell {root}>Dolor</Table.Cell>
+        </Table.Row.Base>
+        <Table.Row.Base {root}>
+            <Table.Cell {root}>Lorem</Table.Cell>
+            <Table.Cell {root}>Ipsum</Table.Cell>
+            <Table.Cell {root}>Dolor</Table.Cell>
+        </Table.Row.Base>
+        <Table.Row.Base {root}>
+            <Table.Cell {root}>Lorem</Table.Cell>
+            <Table.Cell {root}>Ipsum</Table.Cell>
+            <Table.Cell {root}>Dolor</Table.Cell>
         </Table.Row.Base>
     </Table.Root>
 </Story>
