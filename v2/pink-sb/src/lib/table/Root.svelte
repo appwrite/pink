19abcc42125e8dc8a6cd57bc7b880d704ee0b250
Arman <arman@appwrite.io>
2025-03-25 16:51:50 +0100

    fix: columns

--- v2/pink-sb/src/lib/table/Root.svelte
+++ v2/pink-sb/src/lib/table/Root.svelte
@@ -14,7 +14,7 @@
         }
         return cols.reduce(
             (acc, column) => {
-                if (column.show === false) return acc;
+                if (!column?.show) return acc;
                 if (column.width === undefined) return `${acc} 1fr`;
                 if (typeof column.width === 'number') {
                     return `${acc} ${column.width}px`;
