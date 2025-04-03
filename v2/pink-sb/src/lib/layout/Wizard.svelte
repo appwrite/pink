b0eb378f6cdcf38f616ed438f17b45dd3b61bab4
Jesse Winton <jesse@appwrite.io>
2025-04-03 17:43:08 -0400

    Update Wizard.svelte

--- v2/pink-sb/src/lib/layout/Wizard.svelte
+++ v2/pink-sb/src/lib/layout/Wizard.svelte
@@ -21,6 +21,7 @@
     function updateScroll() {
         scrollY = wizardElement.scrollTop;
         updateHasScroll();
+    }
 
     function updateHasScroll() {
         hasScroll = scrollY > 0 || !!title;
