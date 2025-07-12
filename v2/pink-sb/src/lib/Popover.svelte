2f1e67d2ab50372e03dc1e3ccee5125916175bfb
Darshan <itznotabug@gmail.com>
2025-07-12 13:29:44 +0530

    fix: lint.

--- v2/pink-sb/src/lib/Popover.svelte
+++ v2/pink-sb/src/lib/Popover.svelte
@@ -2,7 +2,7 @@
     import { fade } from 'svelte/transition';
     import { activePopover } from './context.js';
     import type { Placement } from '@floating-ui/dom';
-    import { hasContext, tick, onDestroy } from 'svelte';
+    import { hasContext, onDestroy } from 'svelte';
     import { computePosition, autoUpdate, shift, offset, flip } from '@floating-ui/dom';
 
     export let portal: boolean = false;
