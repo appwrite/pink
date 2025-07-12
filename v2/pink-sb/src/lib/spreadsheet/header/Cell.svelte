ffd1c00d7a1dea35aef30fec67ab290549339db1
Darshan <itznotabug@gmail.com>
2025-07-12 19:24:00 +0530

    update: forward context menu events.

--- v2/pink-sb/src/lib/spreadsheet/header/Cell.svelte
+++ v2/pink-sb/src/lib/spreadsheet/header/Cell.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
     import Cell from '../Cell.svelte';
-    import type { ComponentProps } from 'svelte';
+    import { type ComponentProps, createEventDispatcher } from 'svelte';
 
     type $$Props = ComponentProps<Cell>;
     export let root: $$Props['root'];
     export let column: $$Props['column'];
     export let icon: $$Props['icon'] | undefined = undefined;
+
+    const dispatch = createEventDispatcher();
+
+    function forward(event: CustomEvent) {
+        dispatch(event.type, event.detail);
+    }
 </script>
 
-<Cell isHeader {icon} {column} {root} {...$$restProps}><slot /></Cell>
+<Cell isHeader {icon} {column} {root} {...$$restProps} on:contextmenu={forward}><slot /></Cell>
